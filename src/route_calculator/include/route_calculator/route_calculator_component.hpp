3573c4228824b85e73f5de6672d318d17b6c422e
Haru <ikushima.haruaki.xd@tut.jp>
2024-02-08 15:52:20 +0900

    pub_sub_2

--- src/route_calculator/include/route_calculator/route_calculator_component.hpp
+++ src/route_calculator/include/route_calculator/route_calculator_component.hpp
@@ -2,6 +2,9 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <my_msgs/msg/route_request.hpp>
 
 namespace tlab
 {
@@ -9,18 +12,39 @@ namespace tlab
 class RouteCalculator : public rclcpp::Node {
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subscription;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscription;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_subscription;
+    rclcpp::Subscription<my_msgs::msg::RouteRequest>::SharedPtr route_request_subscription;
+
 public:
     RouteCalculator(const rclcpp::NodeOptions& options) : RouteCalculator("", options) {}
-    RouteCalculator(const std::string& name_space = "", const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : 
-        Node("route_calcurator_node", name_space, options)
+    RouteCalculator(const std::string& name_space = "", const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : Node("route_calcurator_node", name_space, options)
     {
-        _subscription = this->create_subscription<std_msgs::msg::String>(
-            "topic_test",
-            rclcpp::QoS(10),
-            [this](const std_msgs::msg::String::SharedPtr msg){
-            RCLCPP_INFO(this->get_logger(), "I heard: %s", msg->data.c_str());
-            }
-        );
+        // _subscription = this->create_subscription<std_msgs::msg::String>(
+        //     "topic_test",
+        //     rclcpp::QoS(10),
+        //     [this](const std_msgs::msg::String::SharedPtr msg){
+        //     RCLCPP_INFO(this->get_logger(), "I heard: %s", msg->data.c_str());
+        //     }
+        // );
+
+        // pose_subscription = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+        //     "pose_topic_test",
+        //     rclcpp::QoS(10),
+        //     [&](const geometry_msgs::msg::PoseStamped::SharedPtr pose_msg){
+        //     RCLCPP_INFO(this->get_logger(), "I heard: %lf", pose_msg->pose.position.x);
+        //     }
+        // );
+
+        // path_subscription = this->create_subscription<nav_msgs::msg::Path>("path_topic_test", rclcpp::QoS(10), [&](const nav_msgs::msg::Path::SharedPtr path_msg) {
+        //     RCLCPP_INFO(this->get_logger(), "I heard: %lf", path_msg->poses.back().pose.position.x);
+        //     // RCLCPP_INFO(this->get_logger(), "I heard: %lf", path_msg->poses[0].pose.position.x);
+        // });
+
+        route_request_subscription = this->create_subscription<my_msgs::msg::RouteRequest>("route_request_topic_test", rclcpp::QoS(10), [&](const my_msgs::msg::RouteRequest::SharedPtr route_request_msg) {
+            RCLCPP_INFO(this->get_logger(), "I heard: %lf", route_request_msg->goal.pose.position.x);
+            // RCLCPP_INFO(this->get_logger(), "I heard: %lf", path_msg->poses[0].pose.position.x);
+        });
     }
 };
 
--- src/route_controller/include/route_controller/route_controller_component.hpp
+++ src/route_controller/include/route_controller/route_controller_component.hpp
@@ -3,6 +3,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <my_msgs/msg/route_request.hpp>
 
 namespace tlab
 {
@@ -10,24 +13,67 @@ namespace tlab
 class RouteController : public rclcpp::Node {
 private:
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
+    rclcpp::Publisher<my_msgs::msg::RouteRequest>::SharedPtr route_request_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     size_t count_;
+    double x_value_ = 0.0;
+
+    std::vector<nav_msgs::msg::Path> all_robot_path;
+
 public:
     RouteController(const rclcpp::NodeOptions& options) : RouteController("", options) {}
-    RouteController(const std::string& name_space = "", const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : 
-        Node("route_controller_node", name_space, options)
+    RouteController(const std::string& name_space = "", const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : Node("route_controller_node", name_space, options), all_robot_path(3)
     {
         using namespace std::chrono_literals;
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic_test",rclcpp::QoS(10));
-        timer_ = this->create_wall_timer(
-            500ms,
-            [&](){
-                auto msg = std::make_shared<std_msgs::msg::String>();
-                msg->data = "Hello " + std::to_string(count_++);
-                RCLCPP_INFO(this->get_logger(), "Pub:%s", msg->data.c_str());
-                publisher_->publish(*msg);
+        // publisher_ =
+        // this->create_publisher<std_msgs::msg::String>("topic_test",rclcpp::QoS(10));
+        pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose_topic_test", rclcpp::QoS(10));
+        path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("path_topic_test", rclcpp::QoS(10));
+        route_request_publisher_ = this->create_publisher<my_msgs::msg::RouteRequest>("route_request_topic_test", rclcpp::QoS(10));
+        static auto path_msg = std::make_shared<nav_msgs::msg::Path>();
+        path_msg->header.frame_id = "map"; // 基準座標を決めている、送るときだけ
+
+        timer_ = this->create_wall_timer(500ms, [&]() {
+            // auto msg = std::make_shared<std_msgs::msg::String>();
+            // msg->data = "Hello " + std::to_string(count_++);
+            // RCLCPP_INFO(this->get_logger(), "Pub:%s", msg->data.c_str());
+            // publisher_->publish(*msg);
+
+            // auto pose_msg =
+            // std::make_shared<geometry_msgs::msg::PoseStamped>();
+            // pose_msg->header.frame_id = "map";
+            // pose_msg->pose.position.x =11.1;
+            // RCLCPP_INFO(this->get_logger(), "Pub:%lf",
+            // pose_msg->pose.position.x); pose_publisher_->publish(*pose_msg);
+
+            // geometry_msgs::msg::PoseStamped p;
+            // p.pose.position.x = 20.55 + x_value_;
+            // x_value_ += 1.0;
+            // path_msg->poses.push_back(p);
+
+            // RCLCPP_INFO(this->get_logger(), "Pub:%lf", path_msg->poses.back().pose.position.x);
+            // path_publisher_->publish(*path_msg);
+
+            int send_no = 0;
+
+            auto route_request_msg = std::make_shared<my_msgs::msg::RouteRequest>();
+            route_request_msg->robot_name = "r" + std::to_string(send_no);
+            route_request_msg->start.pose.position.x = 2.5;
+            route_request_msg->goal.pose.position.x = 5.5;
+
+            for (int i = 0; i < all_robot_path.size(); i++) {
+                if (send_no != i) {
+                    route_request_msg->other_path.push_back(all_robot_path[i]);
+                }
             }
-        );
+
+            RCLCPP_INFO(this->get_logger(), "Pub:%s", route_request_msg->robot_name.c_str());
+            RCLCPP_INFO(this->get_logger(), "Pub:%lf", route_request_msg->start.pose.position.x);
+            RCLCPP_INFO(this->get_logger(), "Pub:%lf", route_request_msg->goal.pose.position.x);
+            route_request_publisher_->publish(*route_request_msg);
+        });
     }
 };
 
